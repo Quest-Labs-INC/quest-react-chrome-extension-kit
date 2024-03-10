9a5a02341aa39a6c41cac62ada804603f6ec9578
1Gireesh <gireeshbhat68@gamil.com>
2024-03-10 22:40:29 +0530

    Add new files and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import questLogo from './assets/quest_logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const onClick = async () => {
+    //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    //     chrome.scripting.executeScript({
+    //         target: { tabId: tab.id! },
+    //         func: () => {
+    //             document.body.style.backgroundColor = 'green';
+    //         }
+    //     });
+    // }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    const changeColorOnClick = async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: () => {
+            document.body.style.backgroundColor = 'black';
+          }
+        });
+      }
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={questLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+            <h1>Quest Chrome extension kit starter template</h1>
+            <div className="card">
+                <button onClick={() => changeColorOnClick()}>
+                    Change Color
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
-
 export default App
